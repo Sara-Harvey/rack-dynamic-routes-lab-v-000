386e8db08a7539a83abc4dfa19c21213daf53ca3
Sara Harvey <saraharveynyc@gmail.com>
2019-07-25 16:03:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,17 @@
 class Application
 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/songs"
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+
 #only accept this route
 /items/<ITEM NAME>
 
