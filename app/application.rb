935d8c9a80764518c26daf7b22e2ef97b41547f9
Sara Harvey <saraharveynyc@gmail.com>
2019-07-25 16:19:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,33 +16,7 @@ class Application
       resp.write item.price
       resp.status = 200
     end
+    
+  else 
 
-=begin
-      resp.write "You requested the songs"
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
-
-
-#only accept this route
-/items/<ITEM NAME>
-
-else
-  404
-  
-
-
-#if requests, return the price of that item
-  
-  /items/<Item Name>
-  
-  #IF a user requests an item that you don't have, then return a 400 and an error message
-  
-  resp.write song.artist
-    end
- 
-    resp.finish
-  end
-=end
 end
