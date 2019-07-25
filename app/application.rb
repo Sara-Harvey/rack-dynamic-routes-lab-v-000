7ce717fbae23c1a982135be33eac9b7917a24774
Sara Harvey <saraharveynyc@gmail.com>
2019-07-25 16:26:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     item = @@items.find{|i| i.name == item_name}
     if item == nil 
       resp.write "Item not found"
-      resp.status = 404 
+      resp.status = 400 
     else 
       resp.write item.price
       resp.status = 200
