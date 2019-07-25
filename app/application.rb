1edc04b51301f95297a393d2b459b27ad2cff77b
Sara Harvey <saraharveynyc@gmail.com>
2019-07-25 16:23:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path.match("/items/").last
+    if req.path.match(/items/).last
     item_name = req.path.split("/items/").last
     item = @@items.find{|i| i.name == item_name}
     if item == nil 
